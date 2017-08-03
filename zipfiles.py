d4640f57af7a6f3b4d0caaf8204f66759ea1d0e3
graktung <graktung@gmail.com>
2017-08-03 17:43:32 +0700

    del

--- zipfiles.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import os
-
-def zipFile(filename, files):
-	zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-	for file in files:
-		zipf.write(file)
-		os.remove(file)	
-	zipf.close()
